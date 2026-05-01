7af11907e6d30c9ff14a1603136431f24083d24e
mashakulina <masha_kulina@mail.ru>
2026-05-01 21:52:20 +0300

    Добавлена строка для обновления полного пакета программы (менеджер+служба)

--- core/manager_config.py
+++ core/manager_config.py
@@ -3,6 +3,11 @@ GITHUB_RAW_URL = "https://raw.githubusercontent.com/mashakulina/Zapret-DPI-for-S
 
 RELEASES_URL = "https://github.com/mashakulina/Zapret-DPI-for-Steam-Deck/releases/latest/download/"
 
+# Полный пакет обновления (менеджер + служба), манифест на GitHub (raw)
+VERSION_CONFIG = {
+    "version_url": f"{GITHUB_RAW_URL}/version.txt",
+    "current_version": "2.7.7.3",
+}
 
 # Конфигурация менеджера
 MANAGER_CONFIG = {
